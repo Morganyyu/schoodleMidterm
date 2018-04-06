30cb8b90439d51c75b6a72c57f507d642eace3cd
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-04-05 19:32:01 -0700

    style changes

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -7,4 +7,22 @@ $(() => {
       $("<div>").text(`ID: ${participant.id} Email: ${participant.email} \n Name: ${participant.name}`).appendTo($("body"));
     }
   });
+
+  $("main").on('click', '.user', function(e) {
+    const $button = $(this);
+    const $tr = $(this).parents('tr');
+
+    if($button.data('enabled')) {
+      $button.data('enabled', false);
+
+      $button.css("font-weight","bold");
+      $tr.find('input').prop('disabled', '');
+    } else {
+      $button.data('enabled', true);
+      $button.css("font-weight","normal");
+      $tr.find('input').prop('disabled', 'disabled');
+    }
+    console.log($button.data('enabled'));
+
+  });
 });

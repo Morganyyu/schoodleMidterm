dd899bbee8fbe79957c97dfb7a079730a764f457
Dominic-Bartlomowicz <dbartlomowicz@gmail.com>
2018-04-06 15:01:04 -0700

    table logic fixed

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -8,7 +8,43 @@ $(() => {
     }
   });
 
-  $("main").on('click', '.user', function(e) {
+  let user_count = 0;
+
+  $("main").on('click', '.new-part', function(e) {
+     let votelength = $("#voteslength > th").length;
+     let vote_count = 0;
+     const $button = $(this);
+     const $row = $('<tr id="rowtemplate">');
+     console.log(votelength);
+
+     let userRowTemplate = `<tr id="user_${user_count}" class="participant">
+                              <td class="user">
+                                <input name="name" type="text" placeholder="Your Name">
+                                  <br />
+                                <input name="email" type="email" placeholder="Your Email">
+                              </td>
+                            </tr>`;
+
+
+
+
+       let $newRow = $(userRowTemplate);
+       for(i = 0; i < votelength-1; i++){
+
+         let voteBoxTemplate = `<td class="votes">
+                                  <input type="checkbox" value ="${user_count}_${vote_count}">
+                                </td>`;
+
+         $newRow.append(voteBoxTemplate);
+         vote_count++;
+       }
+       $(`table.event-table`).append($newRow);
+       user_count++;
+
+     return e;
+   });
+
+  $("main").on('click', '.new-part', function(e) {
     const $button = $(this);
     const $tr = $(this).parents('tr');
 
@@ -25,17 +61,17 @@ $(() => {
     console.log($button.data('enabled'));
   });
 
-  $("main").on('click', '.new-part', function(e) {
-    const $button = $(this);
-    // console.log($button.data('enabled'));
-    const $rowclone = $("#rowtemplate").clone();
-    $rowclone.appendTo($(".event-table > tbody"));
-    // $rowclone.find('.user')
-    $("input.name").focus;
 
-  });
+function renderUsers(users) {
 
+  let createUsers;
 
+  for(var i = 0; i < users.length; i++) {
+    createUsers = users[i];
+    let $element = createUserElement(createUsers);
+    $('#userlist').prepend($element);
+  }
+}
 
 
 });
